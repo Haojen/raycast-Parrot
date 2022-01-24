6a140e0a158ef46582f13c40038edb50a7b65b67
Haojen <haojenma@gmail.com>
2022-01-24 19:15:20 +0800

    Add timeout

--- src/parrot.tsx
+++ src/parrot.tsx
@@ -4,10 +4,23 @@ import { useEffect, useState } from 'react'
 import { List } from '@raycast/api'
 import querystring from 'querystring'
 
-export default () => {
+class Result {
+    q:string
+    err: string
+    result: string
+    constructor(q: string, result: string, err: string) {
+        this.q = q
+        this.err = err
+        this.result = result
+    }
+}
+let delayFetchTranslateAPITimer:NodeJS.Timeout
+export default function () {
     const [inputState, setInputState] = useState<string>()
 
     useEffect(() => {
+        if (!inputState || inputState.trim().length === 0) return
+
         const APP_ID = '0d68776be7e9be0b'
         const APP_KEY = 'MIbu7DGsOPdbatL9KmgycGx0qDOzQWCM'
 
@@ -15,25 +28,33 @@ export default () => {
         const salt = 1039057373 // change to UUID
         const timestamp = Math.round(new Date().getTime() / 1000)
         const sha256 = crypto.createHash('sha256')
-        const sign = sha256.update(APP_ID + inputQueryText + salt + timestamp + APP_KEY).digest('hex')
+        const sha256Content = APP_ID + inputQueryText + salt + timestamp + APP_KEY
+        const sign = sha256.update(sha256Content).digest('hex')
 
-        axios.post('https://openapi.youdao.com/api', querystring.stringify({
-            sign,
-            signType: 'v3',
-            salt, // UUID
-            q: inputQueryText,
-            from: 'en',
-            to: 'zh-CHS',
-            appKey: APP_ID,
-            curtime: timestamp
-        }))
-        .then( (res) => {
-            console.log(res.data, 'res')
-        })
+        clearTimeout(delayFetchTranslateAPITimer)
+        delayFetchTranslateAPITimer = setTimeout(() => {
+            axios.post('https://openapi.youdao.com/api', querystring.stringify({
+                sign,
+                salt,
+                from: 'en',
+                to: 'zh-CHS',
+                signType: 'v3',
+                appKey: APP_ID,
+                q: inputQueryText,
+                curtime: timestamp
+            }))
+                .then( ({data}) => {
+                    console.table(new Result(data?.query, data?.translation, data?.errorCode))
+                })
+        }, 1000)
     }, [inputState])
 
+    function updateInputState(inputText:string) {
+        inputText.trim().length > 0 && setInputState(inputText)
+    }
+
     return (
-        <List onSearchTextChange={ inputText => setInputState(inputText) }>
+        <List onSearchTextChange={ inputText => updateInputState(inputText) }>
             <List.Item title={'red'}/>
         </List>
     )
